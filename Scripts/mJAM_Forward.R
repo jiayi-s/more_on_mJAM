0f4d5c637716d71278af335bc1f79fcef2d353ee
jiayi-s <shenjiayi0809@gmail.com>
2023-02-14 10:43:33 -0800

    added option to define user-selected index

--- Scripts/mJAM_Forward.R
+++ Scripts/mJAM_Forward.R
@@ -1,48 +1,72 @@
-###############################################################################
-## This script is for mJAM (multi-ethnic Joint Analysis of Marginal statistics) 
-## with Forward selection as the model selection method.
+#' Run mJAM with Forward Selection
+#'
+#' @description fitting mJAM-Forward
+#'
+#' @param N_GWAS A vector of sample sizes in all original GWAS studies.
+#' @param X_ref A list of matrices with individual-level SNP dosage data in each study/population. Each column corresponds to a SNP. Note that the columns name should match exactly to the SNP column in `Marg_Result` and `EAF_Result`. If certain SNP(s) is missing in dosage, then insert NAs in corresponding column(s).
+#' @param Marg_Result A data frame with marginal summary statistics from all studies. Col1: SNP name; Col2: Effect sizes from study #1; Col3: Std Errors of effect sizes from study #1; ...
+#' @param EAF_Result A data frame with effect allele frequency (EAF) from all studies. Col1: SNP name; Col2: EAF from study #1; Col3: EAF from study #2; ...
+#' @param condp_cut Threshold of conditional p-value to be considered as significant. No default specified. Usually recommend 5e-8.
+#' @param index_snps Pre-identified index SNP(s), if any. Default is `NULL` which means mJAM-Forward will automatically select index variants.
+#' @param within_pop_threshold Threshold of absolute correlation with selected index SNP(s) within a single population. If a SNP's correlation with any selected index SNP is greater than this threshold in at least one population, it will be excluded from subsequent rounds of index SNP selection.
+#' @param across_pop_threshold Threshold of absolute correlation with selected index SNP(s) across all populations. If a SNP's correlation with any selected index SNP is greater than this threshold in all populations, it will be excluded from subsequent rounds of index SNP selection.
+#' @param coverage The required coverage of credible sets. Default is 0.95.
+#' @param Pr_Med_cut Cut off of mJAM posterior mediation probability (P(Med)) during credible set construction. Low P(Med) may indicate low correlation between the candidate SNP and the index SNP. Any candidate credible set SNPs with P(Med) < Pr_Med_cut will be not be considered for credible set. Default is 0.
+#' @param filter_rare A logical variable indicating whether to filter rare SNPs before the analysis. Default is `FALSE.` If `TRUE`, then please specify `rare_freq`.
+#' @param rare_freq A vector of frequencies between 0 and 0.5 to specify the minor allele frequency cut-off if you want to filter rare SNPs before the analysis. Please also set `filter_rare` to be TRUE. For example, if there are 3 populations, then rare_freq = c(0.01, 0, 0.01) means SNPs with MAF < 0.01 in pop 1 and MAF < 0.01 in pop 3 will be removed from analysis.
+#'
+#' @import tibble
+#' @import dplyr
+#'
+#' @export
+#'
+#' @author Jiayi Shen
+#'
+#' @returns
+#'
+#' \describe{
+#'    \item{index}{A table listing all the selected index SNP(s) (`SNP`), along with their log10(p-value) conditional on all SNP(s) above (`cond_log10p`), the log10(p-value) conditional on all other index SNP(s) (`final_log10p`), and the p-value threshold used in this analysis (`pcut`).}
+#'    \item{cs}{A table recording various posterior probabilities of all SNPs being considered for credible set SNPs. }
+#'    \item{mJAM_marg_est}{A table with the marginal effect estimates and standard errors of all SNPs under the mJAM model.}
+#'    \item{QC_marg_est}{The complete table of marginal effect estimates using fixed-effect model and mJAM model. For QC purpose only.}
+#' }
+#'
 
-###############  Section 0 : load required functions  #################
 
-## ... ##
 # N_GWAS = c(5000, 5000, 5000)
 # X_ref = list(RefDosage_P1,RefDosage_P2,RefDosage_P3)
 # Marg_Result = MargBeta
 # EAF_Result = EAF
-# condp_cut = 0.05/50; within_pop_threshold = 0.50
-# across_pop_threshold = 0.20
-# filter_rare = TRUE; rare_freq = c(0.2, 0.4, 0.4)
-# Pr_Med_cut = 0
-
-# N_GWAS = N_Sample
-# X_ref = Input_Dosage
-# Marg_Result = Marg_Result
-# EAF_Result = MAF_Result
-# condp_cut = 0.05/N_SNP
+# condp_cut = 0.05/50
+# index_snps = c("rs2")
 # within_pop_threshold = 0.50
 # across_pop_threshold = 0.20
+# coverage = 0.95
 # Pr_Med_cut = 0
+# filter_rare = FALSE
+# rare_freq = NULL
 
-## change the name of subset_EUR and has_dosage SNP
-## ... ##
-
-
-mJAM_Forward <- function(N_GWAS, X_ref, 
-                         Marg_Result, EAF_Result, 
-                         condp_cut = NULL, 
-                         within_pop_threshold = 0.50, 
-                         across_pop_threshold = 0.20, 
+mJAM_Forward <- function(N_GWAS, X_ref,
+                         Marg_Result, EAF_Result,
+                         condp_cut = NULL,
+                         index_snps = NULL,
+                         within_pop_threshold = 0.50,
+                         across_pop_threshold = 0.20,
                          coverage = 0.95,
-                         Pr_Med_cut = 0.2,
-                         filter_rare = FALSE, 
+                         Pr_Med_cut = 0,
+                         filter_rare = FALSE,
                          rare_freq = NULL){
   ## Set parameters
-  library(dplyr)
-  library(tibble)
   N_SNP <- numSNPs <- nrow(Marg_Result)
   if(is.null(condp_cut)){condp_cut <- 0.05/N_SNP}
-  
-  ## if filter_rare, then remove rare SNPs 
+
+  ## Check index_snps is in marker names
+  if(!is.null(index_snps) && sum(!(index_snps %in% Marg_Result$SNP))>0){
+    not_in_index_snps = index_snps[!(index_snps %in% Marg_Result$SNP)]
+    error(paste0(paste0(not_in_index_snps, collapse = ","), "not found in Marg_Result."))
+  }
+
+  ## if filter_rare, then remove rare SNPs
   if(filter_rare == TRUE){
     ## check rare frequency cutoff
     if(is.null(rare_freq)){
@@ -50,7 +74,7 @@ mJAM_Forward <- function(N_GWAS, X_ref,
     }else if(length(rare_freq)!=length(X_ref)){
       stop("Length of rare_freq does not match with the number of populations")
     }else{
-      ## filter SNPs whose MAF < rare_freq in "ALL" population 
+      ## filter SNPs whose MAF < rare_freq in "ALL" population
       rare_freq_matrix = matrix(rep(rare_freq,numSNPs), byrow = TRUE, nrow = numSNPs, ncol = length(rare_freq))
       rare_freq_true = abs(EAF_Result[,2:ncol(EAF_Result)]-0.5)>0.5-rare_freq_matrix
       rare_filter_id = which(rowMeans(rare_freq_true,na.rm=T)==1)
@@ -62,11 +86,11 @@ mJAM_Forward <- function(N_GWAS, X_ref,
       }
     }
   }
-  
+
   if(typeof(filter_rare)!="logical"){
     stop("Please specify filter_rare to be either TRUE or FALSE.")
   }
-  
+
   Original_Input_Dosage <- X_ref
   Input_MarglogOR <- Input_MargSElogOR <- Input_MAF <- feMeta_w <- feMeta_w_x_beta <- vector("list", length(X_ref))
   for(pop in 1:length(X_ref)){
@@ -81,11 +105,11 @@ mJAM_Forward <- function(N_GWAS, X_ref,
   }
   feMeta_se <- sqrt(1/Reduce(`+`,feMeta_w))
   feMeta_mean <- Reduce(`+`,feMeta_w_x_beta)/Reduce(`+`,feMeta_w)
-  
+
   ## --- Check missing data
   numEthnic <- length(Input_MarglogOR)
   missing_tbl <- tibble(missing_ethnic_idx = integer(), missing_snp_idx = integer())
-  
+
   for(i in 1:numEthnic){
     temp_missing_gwas <- union(which(is.na(Input_MarglogOR[[i]])), which(is.na(Input_MargSElogOR[[i]])))
     temp_missing_gwas <- union(temp_missing_gwas, which(is.na(Input_MAF[[i]])))
@@ -98,7 +122,7 @@ mJAM_Forward <- function(N_GWAS, X_ref,
     }
   }
   missing_tbl <- distinct(missing_tbl)
-  
+
   ## --- Calculate the LD matrix of X_ref
   Dosage_cor <- vector("list",length(X_ref))
   for(i in 1:length(X_ref)){
@@ -114,8 +138,8 @@ mJAM_Forward <- function(N_GWAS, X_ref,
       Dosage_cor[[i]] <- cor(X_ref[[i]])^2
     }
   }
-  
-  ## -- Calculate sufficient statistics 
+
+  ## -- Calculate sufficient statistics
   GItGI <- GIty <- yty <- yty_med <- vector("list", numEthnic)
   for (i in 1:numEthnic){
     if (nrow(missing_tbl) > 0 && i%in%missing_tbl$missing_ethnic_idx){
@@ -137,24 +161,24 @@ mJAM_Forward <- function(N_GWAS, X_ref,
       GIty[[i]] <- rep(0, numSNPs)
       GIty[[i]][-temp_missing_snp_idx] <- temp.GIty
     }else{
-      GItGI[[i]] <- get_XtX(N_outcome = N_GWAS[i], Gl = X_ref[[i]], 
+      GItGI[[i]] <- get_XtX(N_outcome = N_GWAS[i], Gl = X_ref[[i]],
                             maf = Input_MAF[[i]])
-      GIty[[i]] <- get_z(maf = Input_MAF[[i]], 
+      GIty[[i]] <- get_z(maf = Input_MAF[[i]],
                          betas = Input_MarglogOR[[i]], N_outcome = N_GWAS[i])
     }
-    temp_yty <- get_yty(maf = Input_MAF[[i]], N_outcome = N_GWAS[i], 
-                        betas = Input_MarglogOR[[i]], 
+    temp_yty <- get_yty(maf = Input_MAF[[i]], N_outcome = N_GWAS[i],
+                        betas = Input_MarglogOR[[i]],
                         betas.se = Input_MargSElogOR[[i]])
     yty[[i]] <- temp_yty$yTy.all
     yty_med[[i]] <- temp_yty$yTy.est
-    colnames(GItGI[[i]]) <- rownames(GItGI[[i]]) <- Marg_Result[,1] 
-    names(GIty[[i]]) <- names(yty[[i]]) <- Marg_Result[,1] 
+    colnames(GItGI[[i]]) <- rownames(GItGI[[i]]) <- Marg_Result[,1]
+    names(GIty[[i]]) <- names(yty[[i]]) <- Marg_Result[,1]
   }
-  ## Identify rare variants 
+  ## Identify rare variants
   rare_pct <- rowMeans(abs(EAF_Result[,2:ncol(EAF_Result)]-0.5)>0.48)
   rare_SNPs <- Marg_Result[which(rare_pct>=0.5),1]
   if(length(rare_SNPs)==0){rare_SNPs <- NULL}
-  
+
   ## Run Forward selection
   iter_count <- 0
   selected_ids <- NULL
@@ -168,13 +192,13 @@ mJAM_Forward <- function(N_GWAS, X_ref,
   GItGI_curr <- GItGI
   GIty_curr <- GIty
   yty_curr <- yty
-  
+
   while(iter_count >= 0){
     ## step 1: select top SNPs in the remaining list
     ## selected_id should be the ID in subset_EUR
     if(length(unique(pruned_snps))==N_SNP){break}
     if(iter_count == 0){Input_id = NULL}else{Input_id = match(selected_ids, subset_EUR)}
-    
+
     ## get the id of rare SNPs in remaining SNPs
     if(sum(rare_SNPs %in% subset_EUR)>0){
       rare_id = match(rare_SNPs, subset_EUR)
@@ -182,40 +206,47 @@ mJAM_Forward <- function(N_GWAS, X_ref,
     }else{
       rare_id = NULL
     }
-    
+
     ## get the conditional p-values of all remaining SNPs
-    newFS_RES <- mJAM_get_condp(GItGI = GItGI_curr, GIty = GIty_curr, yty = yty_curr, 
+    newFS_RES <- mJAM_get_condp(GItGI = GItGI_curr, GIty = GIty_curr, yty = yty_curr,
                                 yty_med = yty_med, N_GWAS = N_GWAS, selected_id = Input_id,
                                 use_median_yty_ethnic = NULL, rare_id = rare_id)
-    
-    ## output mJAM marginal p and meta marginal p 
+
+    ## output mJAM marginal p and meta marginal p
     if(iter_count == 0){
-      marginal_est <- tibble(SNP = subset_EUR, 
-                             mJAM_effect = signif(newFS_RES$effect_est, digits = 3), 
+      marginal_est <- tibble(SNP = subset_EUR,
+                             mJAM_effect = signif(newFS_RES$effect_est, digits = 3),
                              feMeta_effect = feMeta_mean,
                              mJAM_se = signif(newFS_RES$se_est, digits = 3),
-                             feMeta_se = feMeta_se) %>% 
+                             feMeta_se = feMeta_se) %>%
         mutate(mJAM_logp = Rmpfr::pnorm(abs(mJAM_effect/mJAM_se),lower.tail = F,log.p = T)+log(2),
-               feMeta_logp = Rmpfr::pnorm(abs(feMeta_effect/feMeta_se),lower.tail = F,log.p = T)+log(2)) %>% 
-        mutate(mJAM_log10p = signif(mJAM_logp/log(10), 4), 
+               feMeta_logp = Rmpfr::pnorm(abs(feMeta_effect/feMeta_se),lower.tail = F,log.p = T)+log(2)) %>%
+        mutate(mJAM_log10p = signif(mJAM_logp/log(10), 4),
                feMeta_log10p = signif(feMeta_logp/log(10),4))
     }
-    
-    if(newFS_RES$condp_min > log(condp_cut)) {break}
-    
-    curr_index_snp <- subset_EUR[newFS_RES$which_condp_min]
+
+
+    ## determine the index SNP of current round
+    if(is.null(index_snps)){
+      if((newFS_RES$condp_min>log(condp_cut))) {break}
+      curr_index_snp <- subset_EUR[newFS_RES$which_condp_min]
+    }else{
+      if(iter_count >= length(index_snps)) {break}
+      curr_index_snp <- index_snps[iter_count+1]
+    }
+
     selected_ids <- c(selected_ids, curr_index_snp)
-    condp_list <- c(condp_list, newFS_RES$condp_min)
+    condp_list <- c(condp_list, newFS_RES$condp[match(curr_index_snp, subset_EUR)])
     message(paste("No.", iter_count+1,"selected index SNP:", curr_index_snp))
-    
+
     ## step 2: construct credible sets based on the selected SNP
     curr_CS <- mJAM_build_CS(X_id = curr_index_snp,
                              prev_X_list = prev_index_snp,
                              All_id = subset_EUR,
-                             PrCS_weights = "Pr(M_C)", 
+                             PrCS_weights = "Pr(M_C)",
                              coverage = coverage,
-                             GItGI_curr = GItGI_curr, GIty_curr = GIty_curr, 
-                             yty_curr = yty_curr, yty_med = yty_med, 
+                             GItGI_curr = GItGI_curr, GIty_curr = GIty_curr,
+                             yty_curr = yty_curr, yty_med = yty_med,
                              N_GWAS = N_GWAS, rare_SNPs = rare_SNPs,
                              Pr_Med_cut = Pr_Med_cut)
 
@@ -227,9 +258,9 @@ mJAM_Forward <- function(N_GWAS, X_ref,
                                   R = Dosage_cor,
                                   within_thre = within_pop_threshold, across_thre = across_pop_threshold)
     pruned_snps <- c(pruned_snps,has_dosage_SNP[c(curr_LD_snp$remove_within, curr_LD_snp$remove_across)])
-    
-    all_CS <- rbind(all_CS, curr_CS) 
-    
+
+    all_CS <- rbind(all_CS, curr_CS)
+
     ## step 4: update input statistics
     if(length(pruned_snps)>0){
       subset_EUR <- has_dosage_SNP[-match(pruned_snps, has_dosage_SNP)]
@@ -237,12 +268,12 @@ mJAM_Forward <- function(N_GWAS, X_ref,
     subset_EUR <- union(subset_EUR, selected_ids)
     subset_EUR <- has_dosage_SNP[has_dosage_SNP%in%subset_EUR]
     message(paste(length(unique(pruned_snps)), "SNPs got pruned.", length(subset_EUR), "SNPs left."))
-    
+
     if(length(subset_EUR)>1){
       for(e in 1:numEthnic){
-        GItGI_curr[[e]] <- GItGI[[e]][subset_EUR, subset_EUR] 
-        GIty_curr[[e]] <- GIty[[e]][subset_EUR] 
-        yty_curr[[e]] <- yty[[e]][subset_EUR] 
+        GItGI_curr[[e]] <- GItGI[[e]][subset_EUR, subset_EUR]
+        GIty_curr[[e]] <- GIty[[e]][subset_EUR]
+        yty_curr[[e]] <- yty[[e]][subset_EUR]
         colnames(GItGI_curr[[e]]) <- rownames(GItGI_curr[[e]]) <- subset_EUR
         names(GIty_curr[[e]]) <- names(yty_curr[[e]]) <- subset_EUR
       }
@@ -256,32 +287,39 @@ mJAM_Forward <- function(N_GWAS, X_ref,
       prev_index_snp <- c(prev_index_snp, curr_index_snp)
       break
     }
-    
+
   }
   message("Analysis ended.")
-  
+
   if(!is.null(selected_ids)){
     final_condp_selected <- mJAM_get_condp_selected(GItGI = GItGI, GIty = GIty, yty = yty,
                                                     N_GWAS = N_GWAS,yty_med = yty_med,
                                                     selected_id = selected_ids,
                                                     rare_SNPs = rare_SNPs)
-    
+
     if(length(selected_ids)>1){
       finalp = final_condp_selected$condp[,1]
     }else{
       finalp =  final_condp_selected$condp
     }
-    
-    MULTI_index <- tibble(SNP = selected_ids, cond_log10p = condp_list/log(10), 
-                          final_log10p = finalp/log(10), pcut = condp_cut)
+
+    MULTI_index <- tibble(SNP = selected_ids,
+                          b_joint = final_condp_selected$b_joint,
+                          b_joint_var = ifelse(length(selected_ids)==1, final_condp_selected$b_joint_var,
+                                               diag(final_condp_selected$b_joint_var)),
+                          cond_log10p = condp_list/log(10),
+                          final_log10p = finalp/log(10),
+                          pcut = condp_cut)
   }else{
     message("No index SNP selected in this region.")
     MULTI_index = NULL
     all_CS = NULL
   }
-  
-  return(list(index = MULTI_index, 
-              cs = all_CS, 
-              mJAM_marg_est = marginal_est[,c(1,2,4,8)], 
+
+  return(list(index = MULTI_index,
+              cs = all_CS,
+              mJAM_marg_est = marginal_est[,c(1,2,4,8)],
               QC_marg_est = marginal_est))
 }
+
+
